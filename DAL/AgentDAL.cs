ee496ff155acc179fe1d3bbd85cc211d376a4a8a
yitzchakdamen <yitzchakdamen@gmail.com>
2025-06-10 10:56:39 +0300

    final finish

--- DAL/AgentDAL.cs
+++ DAL/AgentDAL.cs
@@ -173,19 +173,5 @@ namespace OperativeDB
             }
             return CountAgentsByStatus;
         }
-
-        // private Agent ProduceAgent(MySqlDataReader reader)
-        // {
-        //     Agent agent = new Agent.Builder()
-        //             .SetId(reader.GetInt32("id"))
-        //             .SetCodeName(reader.GetString("codeName"))
-        //             .SetLocation(reader.GetString("location"))
-        //             .SetRealName(reader.GetString("realName"))
-        //             .SetStatus(reader.GetString("status"))
-        //             .SetMissionsCompleted(reader.GetInt32("missionsCompleted"))
-        //             .Build();
-
-        //     return agent;
-        // }
     }
 }
